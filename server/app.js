ff12359268d527ba1fb7184e3352934ebb908df0
jacktherock <sonawaneabhijeet273@gmail.com>
2023-09-07 10:34:12 +0530

    cd

--- server/app.js
+++ server/app.js
@@ -3,22 +3,22 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 require("dotenv").config()
-require("./src/DB/connection")
-const taskRoute = require("./src/routes/task.route");
+// require("./src/DB/connection")
+// const taskRoute = require("./src/routes/task.route");
 
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+// app.use(bodyParser.raw())
+// app.use(bodyParser.text())
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(bodyParser.raw())
-app.use(bodyParser.text())
+// app.use(cors({
+//     origin: "*"
+// }));
 
-app.use(cors({
-    origin: "*"
-}));
-
-app.use("/api", taskRoute);
+// app.use("/api", taskRoute);
 
 const PORT = process.env.PORT ;
+console.log(PORT)
 
 app.listen(8000, () => {
     console.log(`Server is running on port ${PORT}`)
